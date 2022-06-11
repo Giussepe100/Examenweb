7f53343066f2ce59983e72c99553d4d8fa509892
Giussepe Aguilar <soccergiussepe@gmail.com>
2022-06-11 10:45:27 -0500

    modificaciones para enviar por correo

---
+++ Proyecto/correos.php
@@ -0,0 +1,15 @@
+<?php
+  //este es el correo al cual se le enviara el correo
+  $destinatario="soccergiussepe@gmail.com";
+  $asunto="Libros comprados en Edulibro";
+  $mensaje="Bienvenido a un nuevo mundo lleno de aprendizaje y experiencias ,Aqui tienes tu libro ";
+  $header="Enviado desde la página de Edulibro";
+  //funcion mail
+  mail($destinatario,$asunto,$mensaje,$header);
+  echo "<script>alert('correo enviado exitosamente')</scrip>";
+  echo "<script>setTimeout(\"location.href='Pago.html'\",1000)</scrip>";
+
+
+
+
+?>
