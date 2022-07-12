cb911cbb14b994b322bbf0c5f1a1393154ec4307
LuisCoa <63749857+LuisCoa-12@users.noreply.github.com>
2022-07-12 01:12:24 -0500

    Update home-user.php

--- Proyecto/home-user.php
+++ Proyecto/home-user.php
@@ -6,6 +6,7 @@
 <link rel="stylesheet" href="css/index.css">
 <html lang="es" style="background: url(imagenes/Fondo.jpg) fixed;background-size: cover;">
 <head>
+<meta charset="UTF-8">
     <h1 align="center" id="titulo">EduLibro
         <a href="Carpetasphp/cerrar_sesion.php" style="text-decoration: none; float: right; color: black; font-size: 17px; margin-right: 30px; 
         text-shadow: none; margin-top: 35px;" class="btn">Salir</a>
@@ -31,111 +32,161 @@
     <div class="contenido">
         <div class="libros">
             <div class="portada">
-                <a href="ojala.html" class="imagen-portada">
+                <a href="libros/libro-ojala.html" class="imagen-portada">
                     <img src="imagenes/lib1.jpg" class="imagen">
                 </a>
             </div>
             <div class="links-libro">
-                <a href="ojala.html" class="link-titulo">OJALA</a><br>
+                <a href="libros/libro-ojala.html" class="link-titulo">OJALA</a><br>
                 <a href="biografíaautor.html" class="link-autor">Defreds José. A. Gomez Iglesias</a><br>
-                <a href="ojalaprevisualizacion.html" class="btn">Previsualizar</a><br>
-                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.15</a>
+                <a href="libros/libro-ojala.html" class="btn">Previsualizar</a><br>
+                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.61</a>
             </div>
         </div>
         <div class="libros">
             <div class="portada">
-                <a href="ojala.html" class="imagen-portada">
+                <a href="libros/libro-metamorfosis.html" class="imagen-portada">
                     <img src="imagenes/lib2.jpg" class="imagen">
                 </a>
             </div>
             <div class="links-libro">
-                <a href="ojala.html" class="link-titulo">LA METAMORFOSIS</a><br>
+                <a href="libros/libro-metamorfosis.html" class="link-titulo">LA METAMORFOSIS</a><br>
                 <a href="biografíaautor.html" class="link-autor">Franz Kafka</a><br>
-                <a href="ojalaprevisualizacion.html" class="btn">Previsualizar</a><br>
-                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.20</a>
+                <a href="libros/libro-metamorfosis.html" class="btn">Previsualizar</a><br>
+                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.49.50</a>
             </div>
         </div>
         <div class="libros">
             <div class="portada">
-                <a href="ojala.html" class="imagen-portada">
+                <a href="libros/libro-crimen-y-castigo.html" class="imagen-portada">
                     <img src="imagenes/lib3.jpg" class="imagen">
                 </a>
             </div>
             <div class="links-libro">
-                <a href="ojala.html" class="link-titulo">CRIMEN Y CASTIGO</a><br>
+                <a href="libros/libro-crimen-y-castigo.html" class="link-titulo">CRIMEN Y CASTIGO</a><br>
                 <a href="biografíaautor.html" class="link-autor">Fiódor Dostoyevski</a><br>
-                <a href="ojalaprevisualizacion.html" class="btn">Previsualizar</a><br>
-                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.20</a>
+                <a href="libros/libro-crimen-y-castigo.html" class="btn">Previsualizar</a><br>
+                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.69</a>
             </div>
         </div>
         <div class="libros">
             <div class="portada">
-                <a href="ojala.html" class="imagen-portada">
+                <a href="libros/libro-la-carretera.html" class="imagen-portada">
                     <img src="imagenes/lib4.jpg" class="imagen">
                 </a>
             </div>
             <div class="links-libro">
-                <a href="ojala.html" class="link-titulo">LA CARRETERA</a><br>
+                <a href="libros/libro-la-carretera.html" class="link-titulo">LA CARRETERA</a><br>
                 <a href="biografíaautor.html" class="link-autor">Cormac McCarthy</a><br>
-                <a href="ojalaprevisualizacion.html" class="btn">Previsualizar</a><br>
-                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.20</a>
+                <a href="libros/libro-la-carretera.html" class="btn">Previsualizar</a><br>
+                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.35.10</a>
             </div>
         </div>
+
         <div class="libros">
             <div class="portada">
-                <a href="ojala.html" class="imagen-portada">
+                <a href="libros/libro-ojala.html" class="imagen-portada">
                     <img src="imagenes/lib1.jpg" class="imagen">
                 </a>
             </div>
             <div class="links-libro">
-                <a href="ojala.html" class="link-titulo">OJALA</a><br>
+                <a href="libros/libro-ojala.html" class="link-titulo">OJALA</a><br>
                 <a href="biografíaautor.html" class="link-autor">Defreds José. A. Gomez Iglesias</a><br>
-                <a href="ojalaprevisualizacion.html" class="btn">Previsualizar</a><br>
-                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.15</a>
+                <a href="libros/libro-ojala.html" class="btn">Previsualizar</a><br>
+                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.61</a>
             </div>
         </div>
-
-
         <div class="libros">
             <div class="portada">
-                <a href="ojala.html" class="imagen-portada">
+                <a href="libros/libro-metamorfosis.html" class="imagen-portada">
                     <img src="imagenes/lib2.jpg" class="imagen">
                 </a>
             </div>
             <div class="links-libro">
-                <a href="ojala.html" class="link-titulo">CRIMEN Y CASTIGO</a><br>
-                <a href="biografíaautor.html" class="link-autor">Fiódor Dostoyevski</a><br>
-                <a href="ojalaprevisualizacion.html" class="btn">Previsualizar</a><br>
-                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.20</a>
+                <a href="libros/libro-metamorfosis.html" class="link-titulo">LA METAMORFOSIS</a><br>
+                <a href="biografíaautor.html" class="link-autor">Franz Kafka</a><br>
+                <a href="libros/libro-metamorfosis.html" class="btn">Previsualizar</a><br>
+                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.49.50</a>
             </div>
         </div>
         <div class="libros">
             <div class="portada">
-                <a href="ojala.html" class="imagen-portada">
+                <a href="libros/libro-crimen-y-castigo.html" class="imagen-portada">
                     <img src="imagenes/lib3.jpg" class="imagen">
                 </a>
             </div>
             <div class="links-libro">
-                <a href="ojala.html" class="link-titulo">LA METAMORFOSIS</a><br>
-                <a href="biografíaautor.html" class="link-autor">Franz Kafka</a><br>
-                <a href="ojalaprevisualizacion.html" class="btn">Previsualizar</a><br>
-                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.20</a>
+                <a href="libros/libro-crimen-y-castigo.html" class="link-titulo">CRIMEN Y CASTIGO</a><br>
+                <a href="biografíaautor.html" class="link-autor">Fiódor Dostoyevski</a><br>
+                <a href="libros/libro-crimen-y-castigo.html" class="btn">Previsualizar</a><br>
+                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.69</a>
             </div>
         </div>
         <div class="libros">
             <div class="portada">
-                <a href="ojala.html" class="imagen-portada">
+                <a href="libros/libro-la-carretera.html" class="imagen-portada">
                     <img src="imagenes/lib4.jpg" class="imagen">
                 </a>
             </div>
             <div class="links-libro">
-                <a href="ojala.html" class="link-titulo">LA CARRETERA</a><br>
+                <a href="libros/libro-la-carretera.html" class="link-titulo">LA CARRETERA</a><br>
                 <a href="biografíaautor.html" class="link-autor">Cormac McCarthy</a><br>
-                <a href="ojalaprevisualizacion.html" class="btn">Previsualizar</a><br>
-                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.20</a>
+                <a href="libros/libro-la-carretera.html" class="btn">Previsualizar</a><br>
+                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.35.10</a>
             </div>
         </div>
-
+        <div class="libros">
+            <div class="portada">
+                <a href="libros/libro-ojala.html" class="imagen-portada">
+                    <img src="imagenes/lib1.jpg" class="imagen">
+                </a>
+            </div>
+            <div class="links-libro">
+                <a href="libros/libro-ojala.html" class="link-titulo">OJALA</a><br>
+                <a href="biografíaautor.html" class="link-autor">Defreds José. A. Gomez Iglesias</a><br>
+                <a href="libros/libro-ojala.html" class="btn">Previsualizar</a><br>
+                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.61</a>
+            </div>
+        </div>
+        <div class="libros">
+            <div class="portada">
+                <a href="libros/libro-metamorfosis.html" class="imagen-portada">
+                    <img src="imagenes/lib2.jpg" class="imagen">
+                </a>
+            </div>
+            <div class="links-libro">
+                <a href="libros/libro-metamorfosis.html" class="link-titulo">LA METAMORFOSIS</a><br>
+                <a href="biografíaautor.html" class="link-autor">Franz Kafka</a><br>
+                <a href="libros/libro-metamorfosis.html" class="btn">Previsualizar</a><br>
+                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.49.50</a>
+            </div>
+        </div>
+        <div class="libros">
+            <div class="portada">
+                <a href="libros/libro-crimen-y-castigo.html" class="imagen-portada">
+                    <img src="imagenes/lib3.jpg" class="imagen">
+                </a>
+            </div>
+            <div class="links-libro">
+                <a href="libros/libro-crimen-y-castigo.html" class="link-titulo">CRIMEN Y CASTIGO</a><br>
+                <a href="biografíaautor.html" class="link-autor">Fiódor Dostoyevski</a><br>
+                <a href="libros/libro-crimen-y-castigo.html" class="btn">Previsualizar</a><br>
+                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.69</a>
+            </div>
+        </div>
+        <div class="libros">
+            <div class="portada">
+                <a href="libros/libro-la-carretera.html" class="imagen-portada">
+                    <img src="imagenes/lib4.jpg" class="imagen">
+                </a>
+            </div>
+            <div class="links-libro">
+                <a href="libros/libro-la-carretera.html" class="link-titulo">LA CARRETERA</a><br>
+                <a href="biografíaautor.html" class="link-autor">Cormac McCarthy</a><br>
+                <a href="libros/libro-la-carretera.html" class="btn">Previsualizar</a><br>
+                <a href="Pago.html" class="btn"><img src="imagenes/carrito.png" width="auto" height="20px" align="center">&nbsp;Agregar S/.35.10</a>
+            </div>
+        </div>
     </div>
 </body>
 
